b28170a7de5943d3ceec35d01553966ef75c57e1
EJDiamond <emilydiamond321@gmail.com>
2022-10-11 20:44:56 +0000

    Create Admin panel and room model

--- hotel/models.py
+++ hotel/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Room(models.Model):
+    ROOM_CATEGORIES = (
+        ('PSR', 'Poolside'),
+        ('GVR', 'Garden View'),
+        ('OVR', 'Ocean View'),
+        ('SOS', 'Superior Ocean Suite')
+    )
+
+    number = models.IntegerField()
+    category = models.CharField(max_length=3, choices=ROOM_CATEGORIES)
+    adults = models.IntegerField()
+    children = models.IntegerField()
+
+    def __str__(self):
+        return f'{self.number}. {self.category} for {self.adults} adults and {self.children} children'
