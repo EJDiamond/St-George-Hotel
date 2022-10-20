0c7d9d15e05e7c9f82dedec6b70848c7d1681911
EJDiamond <emilydiamond321@gmail.com>
2022-10-20 21:40:45 +0000

    Add customer details form and template

--- hotel/urls.py
+++ hotel/urls.py
@@ -5,5 +5,5 @@ from django.urls import path
 urlpatterns = [
     path('', views.index, name='index'),
     path('booking', views.MakeBooking.as_view(), name='book'),
-
+    path('customer', views.CustomerDetails.as_view(), name='customer'),
 ]
--- hotel/views.py
+++ hotel/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect, reverse
 from django.views import View
 from django.contrib.auth.models import User
 from .models import Booking, Customer
-from .forms import BookingForm
+from .forms import BookingForm, CustomerForm
 from django.http import HttpResponseRedirect
 from django.contrib.auth.mixins import LoginRequiredMixin
 
@@ -24,6 +24,24 @@ class MakeBooking(LoginRequiredMixin, View):
 
     def post(self, request, *args, **kwargs):
         form = self.booking_form(request.POST)
+        if form.is_valid():
+            form.save()
+            return render(request, self.template_name, {'form': form})
+
+
+class CustomerDetails(LoginRequiredMixin, View):
+    """ Allows customer to save their personal details """
+    customer_form = CustomerForm
+    template_name = 'customer_details.html'
+    login_url = 'account_login'
+    redirect_field_name = 'index'
+
+    def get(self, request, *args, **kwargs):
+        form = self.customer_form()
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request, *args, **kwargs):
+        form = self.customer_form(request.POST)
         if form.is_valid():
             form.save()
             return render(request, self.template_name, {'form': form})
